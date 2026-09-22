1cfb951b42079addc082fb727423404c88c4dd18
Rémi Campistron <remi.campistron@fms-ea.com>
2026-09-22 13:31:14 +0200

    Affichage des données mis en forme des trois villes

--- main.py
+++ main.py
@@ -15,52 +15,49 @@ country_code = "FR"
 #dictionnaire des villes
 
 cities_dict = [
-      {
-        "name" : "St geours de Maremne",
-        "lat" : "",
-        "lon" : "",
-        "zip" : "40230",
-        "temp_min" : "",
-        "temp_max" : ""
-     },
-     {
-        "name" : "Toulouse",
-        "lat" :"",
-        "lon" :"",
-        "zip" : "31000",
-        "temp_min" : "",
-        "temp_max" : ""
+    {
+        "name": "St geours de Maremne",
+        "lat": "",
+        "lon": "",
+        "zip": "40230",
+        "temp_min": "",
+        "temp_max": ""
+    },
+    {
+        "name": "Toulouse",
+        "lat": "",
+        "lon": "",
+        "zip": "31000",
+        "temp_min": "",
+        "temp_max": ""
     },
 
-      {
-        "name" : "Mérignac",
+    {
+        "name": "Mérignac",
         "lat": "",
         "lon": "",
         "zip": "33700",
-        "temp_min" : "",
-        "temp_max" : ""
+        "temp_min": "",
+        "temp_max": ""
     }
 ]
 
-for city in cities_dict :
-    coordinates_results = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={city["zip"]},{country_code}&appid={api_key}')
+for city in cities_dict:
+    coordinates_results = requests.get(
+        f'http://api.openweathermap.org/geo/1.0/zip?zip={city["zip"]},{country_code}&appid={api_key}')
     coordinates_data = coordinates_results.json()
     city["lat"] = coordinates_data["lat"]
     city["lon"] = coordinates_data["lon"]
     # Consomme l'api pour la météo
-    weather_results = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat={city["lat"]}&lon={city["lon"]}&appid={api_key}&units=metric&lang=fr')
+    weather_results = requests.get(
+        f'http://api.openweathermap.org/data/2.5/forecast?lat={city["lat"]}&lon={city["lon"]}&appid={api_key}&units=metric&lang=fr')
     weather_data = weather_results.json()
     city["temp_min"] = weather_data["list"][0]["main"]["temp_min"]
     city["temp_max"] = weather_data["list"][0]["main"]["temp_max"]
 
-
-
-
-
-
-
-print(cities_dict)
-
-
-
-
+for city in cities_dict:
+    print("--------------")
+    print(f'{city["name"]}')
+    print("--------------")
+    print(f'min :{city["temp_min"]} °C')
+    print(f'max :{city["temp_max"]} °C')
